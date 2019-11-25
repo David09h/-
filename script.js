e490976abb3519e503ac05589ee8746a9f15ae1f
David <example@gmail.com>
2019-11-25 20:53:42 +0700

    second

---
+++ script.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/*let person={
+    name:"john",
+    age:25,
+    isMarried: false
+}
+
+console.log(person.name)
+
+let arr=['plum.png','orange.jpg'];
+
+console.log(arr[0])
+
+
+
+let answer = +prompt("Есть ли вам 18?m","да")
+
+
+console.log(typeof(answer));
+
+console.log("arr"+"- object")
+console.log("arr"+1)
+*/
+
+let incr = 10,
+    decr = 10;
+
+
+    incr++;
+    decr--;
+    console.log(incr);
+
+    console.log("2"==2);
